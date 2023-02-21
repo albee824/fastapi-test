5b09542d97395b6b64ebde05af2ecd69762d1d38
Yuya Takahashi <yuya.takahashi@spyeye.jp>
2023-02-21 17:42:08 +0900

    Delete endpoint item

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
 @app.get("/calc/double/{num}")
 def read_item(num: int):
     return {"num": num, "answer": num * 2}
